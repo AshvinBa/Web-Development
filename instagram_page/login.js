1738c02526ff29817fb281094f16a3a0a9b7892f
Ashvin Bari <ashvinbari6@gmail.com>
2023-11-17 23:43:54 +0530

    commited
    17-11-2023

---
+++ instagram_page/login.js
@@ -0,0 +1,10 @@
+function myfunction()
+{
+var name=document.getElementById("username").value;
+var pass1=document.getElementById("password").value;
+alert("Your Name and Password are succesfuly saved");
+
+var n=name,p=pass1;
+
+console.log("Name: "+n+" Password: "+p);
+}
