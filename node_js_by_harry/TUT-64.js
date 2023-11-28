92ec8edb301f082324d91363fe86d55877260aa5
Ashvin Bari <ashvinbari6@gmail.com>
2023-11-28 23:36:13 +0530

    commited
    27-11-2023

---
+++ node_js_by_harry/TUT-64.js
@@ -0,0 +1,32 @@
+/*
+const fs=require("fs");
+let text=fs.readFileSync("ash.txt","utf-8");  //To read the content of the file.(file name,buffer argument)
+//console.log(text);  // to print the in the file.
+
+text = text.replace("jeene","peene");   // to replace the word in the text
+console.log(text);
+console.log("creating new file");
+fs.writeFileSync("rep.txt",text); // To make the new file 
+*/
+
+
+/*
+//Read text
+
+const fs=require("fs");
+const text=fs.readFileSync("TUT64.txt","utf-8");
+console.log(text);
+*/
+
+/*
+
+//Write text or replace the text
+
+const fs=require("fs");
+let text=fs.readFileSync("TUT64.txt","utf-8");
+console.log(text);
+
+console.log("-------------------");
+text=text.replace("ipsum","corresponding");
+console.log(text);
+*/
---
+++ node_js_by_harry/gride.js
@@ -0,0 +1,11 @@
+function fun()
+{
+    var ashvin=document.getElementById("dark").innerHTML;
+}
+
+function myfunction()
+{
+    var ele=document.body;
+    ele.classList.toggle("dark-mode");
+    fun();
+}
